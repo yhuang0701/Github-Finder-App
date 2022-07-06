0b924a58ddf823790f5238d3a1ad76832a1b87b8
Yichen Huang <achen765223419@gmail.com>
2022-07-05 17:42:16 -0700

    Add Footer component and update main content layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>Content</main>
+        <main className="container mx-auto px-3 pb-12">Content</main>
+
+        <Footer />
       </div>
     </Router>
   );
