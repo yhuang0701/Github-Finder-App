c52f304213f1ee5e77f8fb06523fbe802cfd0ef5
Yichen Huang <achen765223419@gmail.com>
2022-07-07 16:31:57 -0700

    Update Alert functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/layouts/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
+import Alert from './components/layouts/Alert';
 import { GithubProvider } from './context/github/GithubContext';
 import { AlertProvider } from './context/alert/AlertContext';
 
@@ -14,7 +15,9 @@ function App() {
         <Router>
           <div className="flex flex-col justify-between h-screen">
             <Navbar />
+
             <main className="container mx-auto px-3 pb-12">
+              <Alert />
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,6 +1,6 @@
-import { clear } from '@testing-library/user-event/dist/clear';
 import { useState, useContext } from 'react';
 import GithubContext from '../../context/github/GithubContext';
+import AlertContext from '../../context/alert/AlertContext';
 
 function UserSearch() {
 
@@ -8,13 +8,15 @@ function UserSearch() {
 
     const { users, searchUsers, clearSearch } = useContext(GithubContext);
 
+    const { setAlert } = useContext(AlertContext);
+
     const handleChange = (e) => setText(e.target.value);
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         if (text === "") {
-            alert("Please enter something");
+            setAlert("Please enter something", "error");
         } else {
             searchUsers(text);
 
